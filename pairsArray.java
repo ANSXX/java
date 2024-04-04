5ce69ea5e3e461725167a5ccca0e9cefd0e508c1
Himanshu <himanshu1sharma88@gmail.com>
2024-04-04 17:12:08 +0530

    update c_cpp_properties.json, launch.json, settings.json and pairsArray.java

--- pairsArray.java
+++ pairsArray.java
@@ -1,3 +1,20 @@
 public class pairsArray {
+    public static void printPair(int n[]){
+        int k = 0;
+        for(int i=0;i<n.length;i++){
+            int curr = n[i];
+            for(int j=i+1; j<n.length;j++){
+                System.out.print("("+curr+","+n[j]+")");
+                k++;
+            }
+            System.out.println();
+        }
+        System.out.println("total no of pair is "+k);
+    }
+    public static void main(String [] args){
+        int n[]= {1,2,3,4,5};
+        printPair(n);
+        
+    }
     
 }
