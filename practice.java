428c9fdc2163223e470e50809f5054d4da8c7348
Himanshu <himanshu1sharma88@gmail.com>
2024-04-07 08:21:26 +0530

    update practice.class and practice.java

--- practice.java
+++ practice.java
@@ -1,30 +1,14 @@
-// import java.util.*;
-public class practice
-{
-    //In a program, input 3 numbers : A, B and C. You have to output the average of these 3 numbers.
-    public static void main(String args[])
-    {
-    //     Scanner scan =new Scanner(System.in);
-    //     System.out.print("enter the numbers :-");
-    //     int a =scan.nextInt();
-    //     int b =scan.nextInt();
-    //     int c =scan.nextInt();
-    //     int avg = (a+b+c)/3;
-    //     System.out.println("your avrage is:- " +avg);
-    // // In a program, input the side of a square. You have to output the area of the square./
-    
-    //     System.out.println("enter the side");
-    //     int ab=scan.nextInt();
-    //     int area=(ab*ab);
-    //     System.out.print("area of the squre is:-" +area);
-    //     scan.close();
-        byte b=4;
-        char c='a';
-        short s=512;
-        int i = 1000;
-        float f =3.14f;
-        double d =99.9954;
-        double result = (f*b)+(i%c)-(d*s);
-        System.out.print(result);
+public class practice{
+    public static void nForest(int n) {
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        int N = 3;
+        nForest(N);
     }
 }
