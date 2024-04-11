048ce13234dc49a1eb9a0e511a4530dd97359a72
Himanshu <himanshu1sharma88@gmail.com>
2024-04-11 12:05:01 +0530

    update RandomNumber$GuessingGame.class, RandomNumber.class and RandomNumber.java

--- RandomNumber.java
+++ RandomNumber.java
@@ -1,51 +1,60 @@
 import java.util.*;
+
 public class RandomNumber {
-    private static final int MAX_NUMBER = 100;
-    private static int randomNumber(int max)
-    {
-        return (int)(Math.random()*max);
-    }
-    private static void Hint(int num)
-    {
-        System.out.println("Hint the number is: "+(num%2==0?"even":"odd"));
-    }
-    public static void main(int userNum,int rNum){
-        Scanner scan = new Scanner(System.in);
-        int i = 0;
-        System.out.printf("I have choosed the number between 0 - %d can you gess it?",MAX_NUMBER);
-        while (true) {
-            try{
-                i++;
-                if(scan.hasNextInt())
-                {
-                    
-                    int userNum = scan.nextInt();
-                    if(randomNumber(MAX_NUMBER)<userNum)
-                    {                
-                        System.out.println("Choose the smaller number");
-                        
-                    }
-                    else if(randomNumber(MAX_NUMBER)>userNum){
-                        System.out.println("choose the grater number");
-                        
-                    }
-                    else{
-                        System.out.printf("YAY!! you guessed the right number in %d time ",i);
-                        break;
-                    }
+    private static class GuessingGame {
+        private static final int MAX_NUMBER = 100;
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int targetNumber = generateRandomNumber(MAX_NUMBER);
+            int attempts = 0;
+            
+            System.out.println("Welcome to the Guessing Game!");
+            System.out.println("I've picked a number between 0 and " + MAX_NUMBER + ". Try to guess it!");
+
+            while (true) {
+                attempts++;
+                int userGuess = getUserGuess(scanner);
+                
+                if (userGuess == targetNumber) {
+                    System.out.println("Congratulations! You guessed the right number in " + attempts + " attempts.");
+                    break;
+                } else {
+                    provideHint(targetNumber);
+                    promptNextGuess(userGuess, targetNumber);
                 }
             }
-            catch(InputMismatchException e){
+            
+            scanner.close();
+        }
 
-                System.out.println("enter the number ");
-                scan.next();
+        private static int generateRandomNumber(int max) {
+            return (int) (Math.random() * (max + 1));
+        }
+
+        private static int getUserGuess(Scanner scanner) {
+            System.out.print("Enter your guess: ");
+            while (!scanner.hasNextInt()) {
+                System.out.print("Invalid input. Please enter a valid number: ");
+                scanner.next(); // Consume invalid input
+            }
+            return scanner.nextInt();
+        }
+
+        private static void provideHint(int targetNumber) {
+            System.out.println("Hint: The number is " + (targetNumber % 2 == 0 ? "even" : "odd"));
+        }
+
+        private static void promptNextGuess(int userGuess, int targetNumber) {
+            if (userGuess < targetNumber) {
+                System.out.println("Choose a greater number.");
+            } else {
+                System.out.println("Choose a smaller number.");
             }
         }
-        scan.close();
-    }
     }
 
-
-    public static void main(String args[]){
-        
+    public static void main(String args[]) {
+        GuessingGame.main(args);
+    }
 }
